49549c88c83f9c9969806e8beb6e5cba165c8dde
Alberto Fustinoni <alberto@aftnet.net>
2018-01-09 00:12:33 +0900

    Logic

--- LibretroRT.Test/Cores/ParallelN64CoreTest.cs
+++ LibretroRT.Test/Cores/ParallelN64CoreTest.cs
@@ -1,28 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
-//    public class ParallelN64CoreTest : TestBase
-//    {
-//        protected const string RomName = "N64Game.n64";
+    public class ParallelN64CoreTest : TestBase
+    {
+        public static IEnumerable<object[]> RomNames => new List<object[]>
+        {
+            new object[] { "N64Game.n64" },
+        };
 
-//        public ParallelN64CoreTest() : base(() => ParallelN64RT.ParallelN64Core.Instance)
-//        {
-//        }
+        public ParallelN64CoreTest() : base(() => ParallelN64RT.ParallelN64Core.Instance)
+        {
+        }
 
-//        [Theory]
-//        [MemberData(nameof(RomNames))]
-//        public override Task LoadingRomWorks(string romName)
-//        {
-//            return LoadingRomWorksInternal(romName);
-//        }
+        [Theory]
+        [MemberData(nameof(RomNames))]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
 
-//        [Theory]
-//        [MemberData(nameof(RomNames))]
-//        public override Task ExecutionWorks(string romName)
-//        {
-//            return ExecutionWorksInternal(romName);
-//        }
-//    }
+        [Theory]
+        [MemberData(nameof(RomNames))]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
 }
